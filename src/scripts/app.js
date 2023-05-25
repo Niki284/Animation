af644699f9c0950d7a3fb5e22ffebb999957aab4
NikitaDymovich <nikita.dymovich@student.odisee.be>
2023-05-25 22:57:45 +0200

    test van scroll cursor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,4 +5,23 @@ import {cursor} from "./cursor/cursorB";
 gsap.registerPlugin(ScrollTrigger);
 
 scroll();
-cursor();
+/*
+document.addEventListener("scroll" ,(eveS)=> {
+    const select = document.querySelector(".section");
+    console.log(window.scrollY);
+    console.log(eveS);
+    //lement.style.height = `${element.scrollHeight}px`;
+    //const color = 
+    })
+    */
+   //let mousePosition = {x = 0 , y = 0}
+   /*
+    document.addEventListener("mousemove" ,(eveM)=> {
+        const select = document.querySelector(".section");
+        
+        console.log(window.scrollY);
+        console.log(eveM);
+        //lement.style.height = `${element.scrollHeight}px`;
+        //const color = 
+        })
+        */

892a9fe569e5d195b8e312e85053a128eafc2d33
NikitaDymovich <nikita.dymovich@student.odisee.be>
2023-05-25 11:41:19 +0200

    cursor is klaar
    cursor is klaar  op 80%

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,8 @@
 import { gsap } from "gsap";
 import {scroll} from "./scroll/scroll"
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {cursor} from "./cursor/cursorB";
 gsap.registerPlugin(ScrollTrigger);
 
 scroll();
+cursor();

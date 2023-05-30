f4da936e8b373e014a19115d4f3e12d652eec8fa
NikitaDymovich <nikita.dymovich@student.odisee.be>
2023-05-30 22:23:48 +0200

    ververking van css

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,26 +2,7 @@ import { gsap } from "gsap";
 import {scroll} from "./scroll/scroll"
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import {cursor} from "./cursor/cursorB";
+import "./loading/loading";
 gsap.registerPlugin(ScrollTrigger);
 
 scroll();
-/*
-document.addEventListener("scroll" ,(eveS)=> {
-    const select = document.querySelector(".section");
-    console.log(window.scrollY);
-    console.log(eveS);
-    //lement.style.height = `${element.scrollHeight}px`;
-    //const color = 
-    })
-    */
-   //let mousePosition = {x = 0 , y = 0}
-   /*
-    document.addEventListener("mousemove" ,(eveM)=> {
-        const select = document.querySelector(".section");
-        
-        console.log(window.scrollY);
-        console.log(eveM);
-        //lement.style.height = `${element.scrollHeight}px`;
-        //const color = 
-        })
-        */
